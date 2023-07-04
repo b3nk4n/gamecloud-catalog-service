cdef21990cb34594b64c5beb9ea66ee8b49dd9ed
Benjamin Kan <Kan@ibm.com>
2023-07-04 15:01:39 -0700

    Add integration test and MVC slice test

--- src/test/java/de/b3nk4n/gamecloud/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/de/b3nk4n/gamecloud/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,36 @@
 package de.b3nk4n.gamecloud.catalogservice;
 
+import de.b3nk4n.gamecloud.catalogservice.model.Game;
+import de.b3nk4n.gamecloud.catalogservice.model.GameGenre;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Tag("IntegrationTest")
 class CatalogServiceApplicationTests {
 
+    @Autowired
+    WebTestClient webClient;
+
     @Test
-    void contextLoads() {
+    void whenPostRequestThenGameCreated() {
+        var expectedGame = new Game("1234", "FIFA 23", GameGenre.SPORTS, "EA Sports", 39.99);
+
+        webClient.post()
+                .uri("/games")
+                .bodyValue(expectedGame)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Game.class).value(actualGame -> {
+                    assertThat(actualGame).isEqualTo(expectedGame);
+                });
     }
 
 }
---
+++ src/test/java/de/b3nk4n/gamecloud/catalogservice/controller/GameCatalogControllerMvcTest.java
@@ -0,0 +1,33 @@
+package de.b3nk4n.gamecloud.catalogservice.controller;
+
+import de.b3nk4n.gamecloud.catalogservice.exception.GameNotFoundException;
+import de.b3nk4n.gamecloud.catalogservice.service.GameService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.BDDMockito.given;
+
+@WebMvcTest(GameCatalogController.class)
+class GameCatalogControllerMvcTest {
+    @Autowired
+    MockMvc mockedController;
+
+    @MockBean
+    GameService mockedGameService;
+
+    @Test
+    void whenGetGameNotExistingThenShouldReturnNotFound() throws Exception {
+        String gameId = "1234";
+
+        given(mockedGameService.getGame(gameId))
+                .willThrow(GameNotFoundException.class);
+
+        mockedController.perform(MockMvcRequestBuilders.get("/games/" + gameId))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+}
